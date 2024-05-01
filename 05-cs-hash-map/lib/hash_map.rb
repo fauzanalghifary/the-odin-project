9371ec11f2f6db34994a1807d9c6c7055b3ca426
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-05-01 11:09:22 +0700

    cs - hash map

---
+++ 05-cs-hash-map/lib/hash_map.rb
@@ -0,0 +1,87 @@
+require_relative 'hash_map/hasher'
+require_relative 'hash_map/linked_list'
+
+class HashMap
+  attr_reader :length
+
+  def initialize
+    @buckets = Array.new(INITIAL_CAPACITY)
+    @length = 0
+  end
+
+  MAX_LOAD_FACTOR = 0.75
+  INITIAL_CAPACITY = 16
+
+  def get(key)
+    key_index = hash_index(key)
+    entry = @buckets[key_index]&.find_entry(key)
+    entry&.value
+  end
+  alias [] get
+
+  def set(key, value)
+    key_index = hash_index(key)
+    @buckets[key_index] ||= LinkedList.new
+    entry = @buckets[key_index].find_entry(key)
+
+    if entry
+      entry.value = value
+    elsif high_load_factor?
+      grow_capacity
+      set(key, value)
+    else
+      @buckets[key_index].prepend(key, value)
+      @length += 1
+    end
+  end
+  alias []= set
+
+  def key?(key)
+    !!get(key)
+  end
+
+  def remove(key)
+    key_index = hash_index(key)
+    output_value = @buckets[key_index]&.remove(key)
+    @length -= 1 if output_value
+
+    output_value
+  end
+
+  def clear
+    @buckets = Array.new(capacity)
+    @length = 0
+  end
+
+  def entries
+    @buckets.each_with_object([]) do |bucket, array|
+      next unless bucket
+
+      bucket.traverse { |entry| array << [entry.key, entry.value] }
+    end
+  end
+
+  def keys = entries.map(&:first)
+  def values = entries.map(&:last)
+
+  private
+
+  def hash_index(key)
+    index = Hasher.hash(key) % capacity
+    raise ArgumentError if index.negative? || index >= capacity
+
+    index
+  end
+
+  def grow_capacity
+    current_entries = entries
+    @length = 0
+
+    @buckets = Array.new(capacity * 2)
+    current_entries.each { |key, value| set(key, value) }
+  end
+
+  def capacity = @buckets.size
+  def load_factor = length / capacity
+  def high_load_factor? = load_factor > MAX_LOAD_FACTOR
+end
---
+++ 05-cs-hash-map/lib/hash_map/linked_list.rb
@@ -0,0 +1,56 @@
+class HashMap
+  class LinkedList
+    def initialize
+      @head = nil
+    end
+
+    Node = Struct.new(:key, :value, :next_node) do
+      def initialize(key:, value:, next_node: nil) = super(key, value, next_node)
+    end
+    private_constant :Node
+
+    def prepend(key, value)
+      @head = Node.new(key:, value:, next_node: @head)
+    end
+
+    def find_entry(key)
+      find { |node| node.key == key }
+    end
+
+    def remove(key)
+      (@head&.key == key) ? delete_head : delete_node_with_predecessor(key)
+    end
+
+    def traverse
+      current = @head
+
+      while current
+        yield(current)
+        current = current.next_node
+      end
+    end
+
+    private
+
+    def delete_head
+      target = @head
+      @head = @head.next_node
+      target.value
+    end
+
+    def delete_node_with_predecessor(key)
+      predecessor = find { |node| node.next_node&.key == key }
+      return if predecessor.nil?
+
+      target = predecessor.next_node
+      predecessor.next_node = target.next_node
+      target.value
+    end
+
+    def find
+      traverse { |node| return node if yield(node) }
+    end
+  end
+
+  private_constant :LinkedList
+end
